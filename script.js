0328eb51642da506e96c980010f4161e6c2a60a5
zyaka <vlagover@gmail.com>
2022-07-30 15:41:15 -0400

    add function changed with parsefloat

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let a = 1;
 let b = 2;
 const add = function(a, b) {
-    let result = a + b;
-    alert(result);
+    let result = parseFloat(a) + parseFloat(b);
+    return result;
 }
+const input = document.querySelector('#input');
 const output = document.querySelector('#output');
 const digits = document.querySelector('button');
