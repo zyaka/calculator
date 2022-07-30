457087f488c94aca2b5e3d77759baeba02a258c9
zyaka <vlagover@gmail.com>
2022-07-30 13:56:11 -0400

    added query selectors for buttons and output

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-console.log("Hello");
-// this calculator will add, subtract, multiply and divide
-//Adding
-// prompt("Type a number:")
 let a = 1;
 let b = 2;
 const add = function(a, b) {
     let result = a + b;
     alert(result);
 }
+const output = document.querySelector('#output');
+const digits = document.querySelector('button');
