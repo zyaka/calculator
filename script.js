eeb874d45bcd51e3423584ca78e7782183605c3d
zyaka <vlagover@gmail.com>
2022-09-10 14:03:03 -0400

    works ok but doesn't

--- script.js
+++ script.js
@@ -1,51 +1,45 @@
-let a = 1;
-let b = 2;
+let num1;
+let op;
+let triggerCompute = false;
 
-function add(a, b) {
-    return parseFloat(a) + parseFloat(b);
+function prep(op_in) {
+    op = op_in;
+    num1 = document.getElementById("num").value;
+    document.getElementById("num").value = "";
+    if (triggerCompute)
+        compute();
+    triggerComputer = true;
+
 }
 
-function subtract(a, b) {
-    return parseFloat(a) - parseFloat(b);
+
+function compute(symbol) {
+    if (symbol) {
+        triggerCompute = false;
+    }
+    let num2 = document.getElementById("num").value;
+
+    switch (op) {
+        case "+":
+            document.getElementById("num").value = parseFloat(num1) + parseFloat(num2);
+            break;
+        case "-":
+            document.getElementById("num").value = parseFloat(num1) - parseFloat(num2);
+            break;
+        case "*":
+            document.getElementById("num").value = parseFloat(num1) * parseFloat(num2);
+            break;
+        case "/":
+            document.getElementById("num").value = parseFloat(num1) / parseFloat(num2);
+            break;
+
+        default:
+            break;
+    }
+
 }
-const input = document.querySelector('#input');
-const output = document.querySelector('#output');
-const digits = document.querySelectorAll('button');
 
-digits.forEach(digit => {
-    digit.addEventListener('click', function() {
-
-    })
-})
-
-//console log test
-
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// rl.question('What is your name ? ', function(name) {
-//     rl.question('Where do you live ? ', function(country) {
-//         console.log(`${name}, is a citizen of ${country}`);
-//         rl.close();
-//     });
-// });
-
-// rl.on('close', function() {
-//     console.log('\nBYE BYE !!!');
-//     process.exit(0);
-// });
-
-// Gets user input
-var name = prompt("What is your name?");
-var num = prompt("What is your favorite number? ");
-
-// Uses user input to print out information
-document.write(num + "?! That's my favorite number too!");
-document.querySelector("h1").innerHTML = "Hii";
-document.querySelector("#output").innerHTML = "Hello " + name + "!";
-
-// // Prints out the variable type.write("Name is a " + typeof name);
-// document.write("Num is a " + typeof num);
+const numClick = (num_in) => {
+    document.getElementById("num").value += num_in;
+    console.log(num_in);
+}
